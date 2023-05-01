eac010beff63dea46dc807ddab11b5f6b73c2577
martinyde <yde001@gmail.com>
2023-05-01 12:53:19 +0200

    Changed php requirements

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,23 +1,24 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__.'/{src,tests}')
+  ->in(__DIR__.'/{src,tests}')
 ;
 
 $header =<<<'HEADER'
-This file is part of itk-dev/pretix-api-client-php.
-
+This file is part of itk-dev/serviceplatformen.
 (c) 2020 ITK Development
-
 This source file is subject to the MIT license.
 HEADER;
 
-return PhpCsFixer\Config::create()
-    ->setRiskyAllowed(true)
-    ->setRules([
-        '@Symfony' => true,
-        'array_indentation' => true,
-        'header_comment' => ['header' => $header],
-    ])
-    ->setFinder($finder)
-;
+return (new PhpCsFixer\Config())
+  ->setRiskyAllowed(true)
+  ->setRules([
+    '@PSR2' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'header_comment' => ['header' => $header, 'comment_type' => 'PHPDoc'],
+    'list_syntax' => ['syntax' => 'short'],
+    'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
+    'strict_comparison' => true,
+  ])
+  ->setFinder($finder)
+  ;
